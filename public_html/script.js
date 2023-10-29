048dcbc5f12d33fd208baaac28c704a8ffd232d8
Hunter Copening <106700138+justhhunter@users.noreply.github.com>
2023-10-28 22:10:30 -0700

    Create script.js

---
+++ public_html/script.js
@@ -0,0 +1,4 @@
+// Author: Hunter Copening, Seth Perritt
+// Date: October 21
+// Purpose: This javascript file will take care of the webpages general functionality by updating
+// changes in real time.
