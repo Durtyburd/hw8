bb4dd2ff1f5378984d0b07987df60a3790d425c7
Hunter Copening <106700138+justhhunter@users.noreply.github.com>
2023-10-29 16:10:16 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,3 +2,29 @@
 // Date: October 28
 // Purpose: This javascript file will be the server for our chat website, it will store chats and allow
 // us to be reachable.
+
+
+// modules
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+app.use(express.json());
+
+
+// using mongo atlas
+mongoose.connect('to be added later', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+// serve static files
+app.use(express.static('public_html'));
+
+
+
+
+// running server at 3000
+app.listen(80, () => {
+    console.log(`Server is running on port 80`);
+});
