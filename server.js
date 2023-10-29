e931d864b5a00b75acc4e214ff2e4138be0f96c7
Hunter Copening <106700138+justhhunter@users.noreply.github.com>
2023-10-28 22:12:21 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,4 @@
+// Author: Hunter Copening, Seth Perritt
+// Date: October 28
+// Purpose: This javascript file will be the server for our chat website, it will store chats and allow
+// us to be reachable.
